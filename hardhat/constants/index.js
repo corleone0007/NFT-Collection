c03a2a2ed1f92515d5182f03d810cf104a787eb1
corleone0007 <78020103+corleone0007@users.noreply.github.com>
2022-11-12 12:51:18 +0100

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x163e0614e9dDE2A2f2d76764FF9CE614D66e7847";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ my-app/constants/index.js
@@ -0,0 +1,22 @@
+export const abi =[
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+export const NFT_CONTRACT_ADDRESS = "0xA47D43901470c22eFde096894508C4A41db80d0a"
